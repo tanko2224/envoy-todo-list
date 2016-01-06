b298bf190f535b7b188b36c8dfc44adb2d936c2e
tanko2224 <fmolina3@calstatela.edu>
2016-01-05 19:46:01 -0800

    Adding Task and HomeController

---
+++ resources/views/home.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Envoy Todo List</title>
+
+    <!--  Styles  -->
+
+    <!--  Scripts  -->
+
+
+</head>
+<body>
+<div class="container">
+    Test
+</div>
+</body>
+</html>
